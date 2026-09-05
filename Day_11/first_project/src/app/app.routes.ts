b3184adaf2169be42bb7bd8e7fbca21bd6261404
nora579 <noraalaaeldin2004@gmail.com>
2026-09-05 21:27:32 +0300

    Add the Day 11 Task

---
+++ Day_11/first_project/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home component/home.component';
+import { AboutComponent } from './about-component/about-component';
+import { ContactUsComponent } from './contact-us.component/contact-us.component';
+import { NotfoundComponent } from './notfound/notfound';
+
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactUsComponent },
+  { path: '**', component: NotfoundComponent }
+];
